5940d4a1a2da92dc81fbe722bfac0450d154010e
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-16 21:33:44 -0300

    Criando os atributos e a classe contrutor do elevador.

--- src/src/Elevador.java
+++ src/src/Elevador.java
@@ -2,5 +2,13 @@ package src;
 
 public class Elevador {
   private int qtdCapacidade;
+  private int totalAndares;
+  private int andarAtual;
+
+  public Elevador(int capacidade, int totalAndares) {
+    this.qtdCapacidade = capacidade;
+    this.totalAndares = totalAndares;
+    this.andarAtual = 0;
+  }
 
 }
